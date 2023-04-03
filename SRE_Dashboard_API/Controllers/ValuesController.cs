4d612b0eda7ce3191f337a183d14c098bfd238fc
vikrantchauhan31 <85609589+vikrantchauhan31@users.noreply.github.com>
2023-04-03 11:12:59 +0530

    Add project files.

---
+++ SRE_Dashboard_API/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SRE_Dashboard_API.Models;
+
+namespace SRE_Dashboard_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public static async Task<ExampleData> GetApiData()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Authorization", "Bearer YOUR_API_KEY");
+                HttpResponseMessage response = await client.GetAsync("https://api.example.com/data");
+                string responseBody = await response.Content.ReadAsStringAsync();
+                ExampleData exampleData = JsonConvert.DeserializeObject<ExampleData>(responseBody);
+                return exampleData;
+            }
+        }
+    }
+}
---
+++ SRE_Dashboard_API/Models/ExampleData.cs
@@ -0,0 +1,11 @@
+﻿namespace SRE_Dashboard_API.Models
+{
+    public class ExampleData
+    {
+
+            public string Property1 { get; set; }
+            public int Property2 { get; set; }
+   
+
+    }
+}
